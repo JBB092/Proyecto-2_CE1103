4a4f665683ad09c9946f0ae6f8f41a42fe62f49d
JBB092 <jos.barquero@estudiantec.cr>
2023-10-16 09:47:37 -0600

    Envío de expresiones
    Ahora el cliente puede enviar una operación sencilla (dos operandos y un operador) al cliente para ser evaluada por el servidor y enviar la operación y el resultado al cliente

--- Funcional/src/SocketsTCP/TCPClient.java
+++ Funcional/src/SocketsTCP/TCPClient.java
@@ -5,17 +5,34 @@ import java.net.*;
 
 public class TCPClient {
     public static void main(String[] args) {
-        String serverAddress = "192.168.100.17"; // Dirección IP del servidor
+        String serverAddress = "localhost"; // Cambia esto a la dirección IP del servidor si es diferente
         int serverPort = 12345;
 
-        try {
-            Socket socket = new Socket(serverAddress, serverPort);
+        try (Socket socket = new Socket(serverAddress, serverPort);
+             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println("Hola, servidor!");
+            System.out.print("Ingresa el primer operando: ");
+            double operand1 = Double.parseDouble(userInput.readLine());
 
-            out.close();
-            socket.close();
+            System.out.print("Ingresa el operador (+, -, *, /): ");
+            String operator = userInput.readLine();
+
+            System.out.print("Ingresa el segundo operando: ");
+            double operand2 = Double.parseDouble(userInput.readLine());
+
+            // Enviar los operandos y el operador al servidor
+            out.println(operand1);
+            out.println(operator);
+            out.println(operand2);
+
+            // Leer la respuesta del servidor
+            String response;
+            while ((response = in.readLine()) != null) {
+                System.out.println("Respuesta del servidor: " + response);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Funcional/src/SocketsTCP/TCPServer.java
+++ Funcional/src/SocketsTCP/TCPServer.java
@@ -5,24 +5,68 @@ import java.net.*;
 
 public class TCPServer {
     public static void main(String[] args) {
-        int port = 12345;
+        ServerSocket serverSocket = null;
 
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("Servidor esperando conexiones en el puerto " + port);
+            serverSocket = new ServerSocket(12345);
+            System.out.println("Servidor escuchando en el puerto 12345...");
 
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente conectado desde: " + clientSocket.getInetAddress());
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde: " + clientSocket.getInetAddress());
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String message = in.readLine();
-            System.out.println("Mensaje recibido del cliente: " + message);
+                // Configurar los streams de entrada y salida
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-            in.close();
-            clientSocket.close();
-            serverSocket.close();
+                // Leer los operandos y el operador del cliente
+                double operand1 = Double.parseDouble(in.readLine());
+                String operator = in.readLine();
+                double operand2 = Double.parseDouble(in.readLine());
+
+                // Realizar la operación
+                double result;
+                switch (operator) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        if (operand2 == 0) {
+                            out.println("Operación inválida: División por cero.");
+                            clientSocket.close();
+                            continue;
+                        }
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        out.println("Operación inválida: Operador no reconocido.");
+                        clientSocket.close();
+                        continue;
+                }
+
+                // Enviar la operación y el resultado al cliente
+                out.println("Operación: " + operand1 + " " + operator + " " + operand2);
+                out.println("Resultado: " + result);
+
+                // Cerrar la conexión con el cliente
+                clientSocket.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
